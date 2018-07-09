230b1021d20506b2a150d679e3b8273d6f925950
Kingston Chan <c.kingston@univapay.com>
2018-07-09 19:21:51 +0900

    Remove var_dumps
    It causes data to be written to the browser output

--- src/Gopay/Errors/GopayInvalidWebhookData.php
+++ src/Gopay/Errors/GopayInvalidWebhookData.php
@@ -9,9 +9,7 @@ class GopayInvalidWebhookData extends GopayError
 
     public function __construct($payload, $code = 0, Throwable $previous = null)
     {
-        ob_start();
-        var_dump($payload);
-        $payloadAsString = ob_end_clean();
+        $payloadAsString = print_r($payload, true);
         parent::__construct($payloadAsString . " is not valid webhook data", $code, $previous);
     }
 }
--- src/Gopay/Errors/GopayRequestError.php
+++ src/Gopay/Errors/GopayRequestError.php
@@ -15,7 +15,7 @@ class GopayRequestError extends GopayError
         $this->status = $status;
         $this->code = $code;
         $this->errors = $errors;
-        parent::__construct(var_dump(array('url' => $url, 'status' => $status, 'code' => $code, 'errors' => $errors)));
+        parent::__construct(print_r(['url' => $url, 'status' => $status, 'code' => $code, 'errors' => $errors], true));
     }
 
     public static function fromJson($url, $json)
